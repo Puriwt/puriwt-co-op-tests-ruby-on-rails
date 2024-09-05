5fbd74bf9b0f9dfb94bd9308393662168d42e9eb
Puriwt <111586756+Puriwt@users.noreply.github.com>
2024-09-05 13:06:06 +0700

    Migrate database and create user view to see data.

--- app/controllers/registation_controller.rb
+++ app/controllers/registation_controller.rb
@@ -1,13 +1,23 @@
 class RegistationController < ApplicationController
   def index
     @sending = "Register Form"
+    @users = User.all
   end
 
-  def submit_form
-    @name = params[:name]
-    @gender = params[:gender]
-    @car = params[:car]
-    # Handle the form data as needed
-    redirect_to root_path, notice: "Form submitted successfully!"
+  def create
+    @user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      birthdate: params[:birthdate],
+      gender: params[:gender],
+      email: params[:email],
+      phone: params[:phone],
+      subject: params[:subject]
+    )
+    if @user.save
+      redirect_to root_path, notice: "Form submitted successfully!"
+    else
+      render :new
+    end
   end
 end

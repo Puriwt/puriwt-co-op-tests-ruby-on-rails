9dbbe36214e97816f7edb46a3227c3d248a32a20
Puriwt <111586756+Puriwt@users.noreply.github.com>
2024-09-05 12:29:05 +0700

    Create form page.

--- app/controllers/registation_controller.rb
+++ app/controllers/registation_controller.rb
@@ -2,4 +2,12 @@ class RegistationController < ApplicationController
   def index
     @sending = "Register Form"
   end
+
+  def submit_form
+    @name = params[:name]
+    @gender = params[:gender]
+    @car = params[:car]
+    # Handle the form data as needed
+    redirect_to root_path, notice: "Form submitted successfully!"
+  end
 end

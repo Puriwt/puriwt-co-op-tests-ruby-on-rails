6dd850101f76697d71b6cde2c93a92b31232a96c
Puriwt <111586756+Puriwt@users.noreply.github.com>
2024-09-05 23:00:36 +0700

    Push to fix CI.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
@@ -91,6 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.active_record.sqlite3_production_warning= false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [

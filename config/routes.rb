9d9232d521d5a8bd229aa23a59be4849d6cb898c
Puriwt <111586756+Puriwt@users.noreply.github.com>
2024-09-09 20:32:55 +0700

    fix rout nd ruby version to 3.3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "registation#index"
-  resources :registation, only: [:index, :create, :destroy, :edit, :update, :cancle]
+  resources :registation, only: [ :index, :create, :destroy, :edit, :update, :cancle ]
+  post "/submit_form", to: "registation#create"
 end
-
